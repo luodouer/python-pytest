c014e9658dde4cc459c60f7cb44874a9bac716f5
luodouer <1376324108@qq.com>
2019-05-13 22:28:00 +0800

    pytest

--- douzi_0419/run.py
+++ douzi_0419/run.py
@@ -6,5 +6,5 @@ import sys
 import pytest
 
 sys.path.append('./')
-pytest.main(['--html=Results/Reports/report.html', '--junitxml=Results/Reports/report.xml', '-m', 'ff', '--reruns', '2',
-    '--alluredir=Results/allure'])
+pytest.main(['--html=douzi_0419/Results/Reports/report.html', '--junitxml=douzi_0419/Results/Reports/report.xml', '-m', 'ff', '--reruns', '2',
+    '--alluredir=douzi_0419/Results/allure'])
