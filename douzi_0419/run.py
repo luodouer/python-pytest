90d45af727f1062479e2f3889fac66f5270bc638
luodouer <1376324108@qq.com>
2019-05-13 22:41:04 +0800

    修改目录

--- douzi_0419/run.py
+++ douzi_0419/run.py
@@ -6,5 +6,5 @@ import sys
 import pytest
 
 sys.path.append('./')
-pytest.main(['--html=douzi_0419/Results/Reports/report.html', '--junitxml=douzi_0419/Results/Reports/report.xml', '-m', 'ff', '--reruns', '2',
-    '--alluredir=douzi_0419/Results/allure'])
+pytest.main(['--html=douzi_0419/results/reports/report.html', '--junitxml=douzi_0419/results/reports/report.xml', '-m', 'register', '--reruns', '2',
+    '--alluredir=douzi_0419/results/allure'])
